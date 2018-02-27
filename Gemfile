b200d711969e7fea16a10f1742deb2e515511dd9
hvdson <hvdsonmusic@gmail.com>
2018-02-27 20:02:51 +0000

    finished exercise 5

--- Gemfile
+++ Gemfile
@@ -2,5 +2,5 @@
 source "https://rubygems.org"
 
 gem 'pry' # for debugging
-gem "pg", "~> 0.18" 
+gem "pg", "~> 0.18"
 gem 'activerecord'
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,9 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@tnr = Store.sum("annual_revenue")
+@number_of_stores = Store.count
+
+puts "Total Revenue: #{@tnr}, Average annual revenue: #{@tnr / @number_of_stores}"
+
+puts Store.where("annual_revenue > 1000000").count
