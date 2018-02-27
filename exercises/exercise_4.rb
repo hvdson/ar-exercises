d9b64d3510bb755f517f398091dad591c63aa907
hvdson <hvdsonmusic@gmail.com>
2018-02-27 19:54:04 +0000

    finished exercise 4 - needs refactoring for the query selector - how to mix ORM and query string?

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -14,6 +14,14 @@ Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, women
 @mens_stores = Store.where(mens_apparel: true)
 
 @mens_stores.each do |store|
+  puts "PRINTING STORES WITH MEN'S APPAREL: " 
   puts "Store Name: #{store.name}, Annual Revenue: #{store.annual_revenue}"
+end
+
+@womens_stores = Store.where("womens_apparel = true AND annual_revenue < 1000000")
+
+@womens_stores.each do |store| 
+  puts "PRINTING STORES WITH WOMEN'S APPAREL && REVENUE < $1M:"
+  puts "Store Name: #{store.name}, Annual Revenue #{store.annual_revenue}"
 end
 
