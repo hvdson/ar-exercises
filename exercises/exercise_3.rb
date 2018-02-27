c86c752dfca345efbe8466c771ae2292ebb2d70e
hvdson <hvdsonmusic@gmail.com>
2018-02-27 19:39:17 +0000

    finished exercise_3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,11 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+@store3 = Store.find(3)
+@store3.destroy
+
+#this throws error
+# puts Store.find(3)
+
+# outputs 2
+puts Store.count
