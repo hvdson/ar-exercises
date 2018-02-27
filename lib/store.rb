0a7bdf433a704ad7d6d89f688e3f858a0aacc734
hvdson <hvdsonmusic@gmail.com>
2018-02-27 23:01:21 +0000

    kinda finished exercise 9 but idk how to just use callbacks - used dependant thing

--- lib/store.rb
+++ lib/store.rb
@@ -1,5 +1,16 @@
 class Store < ActiveRecord::Base
-  has_many :employees
+  # attr_accessor :destroy
+  has_many :employees, :dependent => :destroy
   validates_length_of :name, minimum: 3
   validates :annual_revenue, :numericality => {greater_than_or_equal_to: 0} 
+
+  before_destroy :has_no_employees?
+
+  private
+    def has_no_employees?
+      # if there's employees in the store
+      # self.employees.empty?
+      self.employees.size < 1
+    end
+
 end
